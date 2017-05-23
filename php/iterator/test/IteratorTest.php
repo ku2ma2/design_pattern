d4bfcd476e0918f7345fadd869b314980c2920b0
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-05-24 07:06:13 +0900

    Add Test for Iterator

---
+++ php/iterator/test/IteratorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require dirname(__DIR__) . '/Iterator.php';
+
+/**
+ * @covers Email
+ */
+final class IteratorTest extends TestCase
+{
+    public function testPHPUnitTest()
+    {
+        $this->assertEquals( 0, false);
+    }
+}
