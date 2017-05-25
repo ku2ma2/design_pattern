8b503f40c94d8d695f9dc90716d2c0c75b809a9c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-05-25 21:39:59 +0900

    Add Comments

--- Iterator/php/Book.php
+++ Iterator/php/Book.php
@@ -3,12 +3,28 @@
 class Book
 {
     private $name;
-  
+
+    /**
+     * コンストラクタ
+     *
+     * 本の名前を与えられてクラス変数に追加する
+     *
+     * @access public
+     * @param String $name 本の名前
+     * @return void
+     */
     public function __construct($name = '')
     {
         $this->name = $name;
     }
 
+    /**
+     * 本の名前取得
+     *
+     * @access public
+     * @param void
+     * @return String $this->name
+     */
     public function getName()
     {
         return $this->name;
