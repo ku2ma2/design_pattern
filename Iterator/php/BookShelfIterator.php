2c84e451454a1962904b241b40e6a577051da0bf
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-05-25 14:10:41 +0900

    Add commend in BookShelfIterator

--- Iterator/php/BookShelfIterator.php
+++ Iterator/php/BookShelfIterator.php
@@ -8,12 +8,32 @@ class BookShelfIterator implements IteratorInterface
     private $bookShelf;
     private $index;
 
+    /**
+     * コンストラクタ
+     *
+     * Bookshelf(本棚)を引数にとって、イテレーターオブジェクトを作成する
+     *
+     * @access public
+     * @param BookShelf
+     * @return void
+     * @see BookShelf::iterator
+     */
     public function __construct(BookShelf $bookShelf)
     {
         $this->bookShelf = $bookShelf;
         $this->index = 0;
     }
-  
+
+    /**
+     * 次の本があるかチェック
+     *
+     * 今指定されているカーソル(index)からBookshelf(本棚)オブジェクトの
+     * 次があるかをチェック
+     *
+     * @access public
+     * @param void
+     * @return Boolean
+     */
     public function hasNext()
     {
         if ($this->index < $this->bookShelf->getLength()) {
