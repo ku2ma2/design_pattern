a065ae85b0ea4ab0c56dc8521efaf28b10bc7dc3
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-05-26 07:47:04 +0900

    Add Test for BookShelfIterator

---
+++ Iterator/php/tests/BookShelfIteratorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once dirname(__DIR__) . '/BookShelfIterator.php';
+
+/**
+ * BookShelf Test
+ */
+final class BookShelfIteratorTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $bookShelf = new BookShelf(3);
+        $bookShelf->appendBook(new Book("book1"));
+        $bookShelf->appendBook(new Book("book2"));
+        $bookShelf->appendBook(new Book("book3"));
+
+        $it = $bookShelf->iterator();
+        $this->assertEquals(is_object($it), true);
+
+        return $it;
+    }
+    /**
+     * @depends testConstruct
+     */
+    public function testNext($it)
+    {
+        $book = $it->next();
+        $this->assertEquals(is_object($book), true);
+        $this->assertEquals($book->getName(), 'book1');
+
+        $book = $it->next();
+        $this->assertEquals(is_object($book), true);
+        $this->assertEquals($book->getName(), 'book2');
+
+        return $it;
+    }
+    /**
+     * @depends testNext
+     */
+    public function testHasNext($it)
+    {
+        $this->assertEquals($it->hasNext(), true);
+        $book = $it->next();
+        // 最終的にはFalseになる
+        $this->assertEquals($it->hasNext(), false);
+    }
+}
