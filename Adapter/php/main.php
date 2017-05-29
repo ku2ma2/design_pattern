39b97b8cfc62514f2c86872fbd3714fbab61438a
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-05-30 07:21:46 +0900

    Add Main for Adapter

--- Adapter/php/main.php
+++ Adapter/php/main.php
@@ -2,15 +2,6 @@
 
 require_once "PrintBanner.php";
 
-$bookShelf = new PRintBanner();
-$bookShelf->appendBook(new Book("book1"));
-$bookShelf->appendBook(new Book("book2"));
-$bookShelf->appendBook(new Book("book3"));
-$bookShelf->appendBook(new Book("book4"));
-$bookShelf->appendBook(new Book("book5"));
-$it = $bookShelf->iterator();
-
-while ($it->hasNext()) {
-    $book = $it->next();
-    echo $book->getName()."\n";
-}
+$p = new PrintBanner("Hello");
+$p->printWeak();
+$p->printStrong();
