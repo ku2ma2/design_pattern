08f27617e40b2b15e2f2d63feee99537385258fe
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-04 08:49:17 +0900

    Add CharDisplay and main

---
+++ Template/php/CharDisplay.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "AbstractDisplay.php";
+
+class CharDisplay extends AbstractDisplay
+{
+    public function open()
+    {
+    }
+    public function print()
+    {
+    }
+    public function close()
+    {
+    }
+}
---
+++ Template/php/main.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "CharDisplay.php";
+
+$d1 = new CharDisplay('H');
+
+
+$d1.display();
