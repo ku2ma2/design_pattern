d992a5c100fc4c9ee645ae78ccb11c8918fe26cd
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-05 07:43:40 +0900

    Add StringDisplay

--- Template/php/CharDisplay.php
+++ Template/php/CharDisplay.php
@@ -4,13 +4,31 @@ require_once "AbstractDisplay.php";
 
 class CharDisplay extends AbstractDisplay
 {
+    private $ch;
+
+    /**
+     * コンストラクタ
+     *
+     * クラス変数に引数として与えられた文字($ch)を格納
+     *
+     * @access public
+     * @param string $ch 文字
+     * @return void
+     */
+    public function __construct($ch)
+    {
+        $this->ch = $ch;
+    }
     public function open()
     {
+        echo "<<";
     }
     public function print()
     {
+        echo $this->ch;
     }
     public function close()
     {
+        echo ">>\n";
     }
 }
