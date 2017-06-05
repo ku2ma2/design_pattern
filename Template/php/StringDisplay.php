ca832a0a13ea97ca86f5bd06f302f494daca2967
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-06 07:42:05 +0900

    Add Comment to CharDisplay and StringDisplay

--- Template/php/StringDisplay.php
+++ Template/php/StringDisplay.php
@@ -21,18 +21,60 @@ class StringDisplay extends AbstractDisplay
         $this->string = $string;
         $this->width = mb_strlen($string, 'UTF-8');
     }
+
+    /**
+     * 最初の処理
+     *
+     * 文字列を開いた際に最初に行われる処理ここではprintLineを呼び出している
+     *
+     * @access public
+     * @param void
+     * @return void
+     * @see StringDisplay::printLine
+     */
     public function open()
     {
         $this->printLine();
     }
+
+    /**
+     * 文字列の出力
+     *
+     * コンストラクタにて与えられた文字列を出力する
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function print()
     {
         echo "|" . $this->string . "|\n";
     }
+
+    /**
+     * 最後の処理
+     *
+     * 文字列を閉じる際に最後に行われる処理ここではprintLineを呼び出している
+     *
+     * @access public
+     * @param void
+     * @return void
+     * @see StringDisplay::printLine
+     */
     public function close()
     {
         $this->printLine();
     }
+
+    /**
+     * 区切り文字の出力
+     *
+     * 文字列は行ごとに出力されるので最初と最後に区切り文字を出力する
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     private function printLine()
     {
         echo "+";
