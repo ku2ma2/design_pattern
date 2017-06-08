3584b204056223df5bf0e709248602e1baa3a7cb
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-09 07:48:55 +0900

    modified IDCard

--- Factory/php/IDCard.php
+++ Factory/php/IDCard.php
@@ -5,7 +5,7 @@ namespace idcard;
 require_once "Product.php";
 
 /**
- * ディスプレイのテンプレート
+ * 認証番号カード
  *
  * 拡張されたサブクラスに指定されたメソッドの実装を強制する
  *
@@ -13,21 +13,6 @@ require_once "Product.php";
  * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
  * @copyright ku2ma2
  */
-abstract class Factory
+abstract class IDCard extends Product
 {
-
-    /**
-     * 商品(Product)作成
-     *
-     * @access public
-     * @final
-     * @param string $owner 所有者名
-     * @return Object $p 商品オブジェクト
-     */
-    final public function create($owner)
-    {
-        $p = $this->createProduct($owner);
-    }
-    abstract protected function createProduct($owner);
-    abstract protected function registerProduct($product);
 }
