d090fda1515f60e6293f13e0e03a4887a18bd1a5
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-11 08:08:47 +0900

    Modified IDCard

--- Factory/php/IDCard.php
+++ Factory/php/IDCard.php
@@ -7,12 +7,27 @@ require_once "Product.php";
 /**
  * 認証番号カード
  *
- * 拡張されたサブクラスに指定されたメソッドの実装を強制する
+ * 製品Productクラスのサブクラスとして認証番号カードを管理する
  *
  * @access public
+ * @extends \framework\Product
  * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
  * @copyright ku2ma2
  */
-abstract class IDCard extends Product
+abstract class IDCard extends \framework\Product
 {
+    private $owner;
+    public function __construct(string $owner)
+    {
+        echo $owner . "のカードを作ります\n";
+        $this->owner = $owner;
+    }
+    public function use()
+    {
+        echo $this->owner . "のカードを使います\n";
+    }
+    public function getOwner()
+    {
+        return $this->owner;
+    }
 }
