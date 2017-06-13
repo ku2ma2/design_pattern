1ad7e7fc08e6ab6aad7eb8d8e338d9ae9c349f99
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-14 07:22:29 +0900

    Finished Singleton
    - Modified main.php
    - Fixed Typo Sigleton -> Singleton

--- Singleton/php/main.php
+++ Singleton/php/main.php
@@ -6,5 +6,10 @@ echo "Start.\n";
 $obj1 = Singleton::getInstance();
 $obj2 = Singleton::getInstance();
 
+if ($obj1 === $obj2) {
+    echo "obj1とobj2は同じインスタンスです。\n";
+} else {
+    echo "obj1とobj2は同じインスタンスではありません。\n";
+}
 
 echo "End.\n";
