6746aa2c3fd0cb8e4f35bcdc74b51e541d9cc2b5
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-18 09:17:17 +0900

    Add comments to Manager

--- Prototype/php/Manager.php
+++ Prototype/php/Manager.php
@@ -3,14 +3,43 @@ namespace prototype;
 
 require_once "Product.php";
 
+/**
+ * インスタンス管理
+ *
+ * Product Interfaceを介して「登録」「複製」を行う
+ *
+ * @access public
+ * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
+ * @copyright ku2ma2
+ */
 class Manager
 {
     private $shocase = [];
 
+    /**
+     * インスタンス登録
+     *
+     * @access public
+     * @param string $name インスタンスの登録名
+     * @param Product $proto 登録をするインスタンス
+     */
     public function register(string $name, Product $proto)
     {
         $this->showcase[$name] = $proto;
     }
+
+    /**
+     * インタンス複製
+     *
+     * Product Interfaceを利用してインスタンスを複製
+     * PHPの場合はjavaと違ってcloneableのような仕組みはないので
+     * createCloneのような実装をせずにそのまま「clone」でも
+     * 良いと思う(ディープコピーの場合の処理は必要になるが)
+     *
+     * @access public
+     * @param string $protoname インスタンスを呼び出す名前
+     * @return Prototype $p->createClone 指定されたインスタンスをクローンする
+     */
     public function create(string $protoname)
     {
         $p = $this->showcase[$protoname];
