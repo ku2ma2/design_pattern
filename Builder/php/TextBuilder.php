8c1716bc6f43c368231d2439a2d53a1f121c859b
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-20 07:47:46 +0900

    Add makeString function in TextBuilder

--- Builder/php/TextBuilder.php
+++ Builder/php/TextBuilder.php
@@ -11,7 +11,13 @@ class TextBuilder
         $this->buffer .= "『{$title}』\n";
         $this->buffer .= "\n";
     }
-    
+
+    public function makeString(string $str)
+    {
+        $this->buffer .= "■{$str}\n";
+        $this->buffer .= "\n";
+    }
+
     public function getResult()
     {
         return $this->buffer;
--- Builder/php/tests/TextBuilderTest.php
+++ Builder/php/tests/TextBuilderTest.php
@@ -9,13 +9,25 @@ require_once dirname(__DIR__) . '/TextBuilder.php';
  */
 final class TextBuilderTest extends TestCase
 {
-    public function testGetResult()
+    public function testMakeTitle()
     {
         $textbuilder = new TextBuilder();
         $textbuilder->makeTitle('Greeting');
 
         $expected = '=============================='."\n";
         $expected .= '『Greeting』'."\n";
+        $expected .= "\n";
+        
+        $this->assertEquals($textbuilder->getResult(), $expected);
+        return $textbuilder;
+    }
+
+    public function testMakeString()
+    {
+        $textbuilder = new TextBuilder();
+        $textbuilder->makeString('お昼にかけて');
+
+        $expected = '■お昼にかけて'."\n";
         $expected .= "\n";
         
         $this->assertEquals($textbuilder->getResult(), $expected);
