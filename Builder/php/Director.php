b0504d50eb9bc4fd8c793986e941ed741009d49b
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-06-25 07:40:18 +0900

    Fixed Director.php

--- Builder/php/Director.php
+++ Builder/php/Director.php
@@ -8,4 +8,20 @@ class Director
     {
         $this->builder = $builder;
     }
+    public function construct()
+    {
+        $this->builder->makeTitle("Greeting"); // タイトル
+        $this->builder->makeString("朝から昼にかけて");
+        $this->builder->makeItems([
+            "おはようございます。",
+            "こんにちは。"
+        ]);
+        $this->builder->makeString("夜に");
+        $this->builder->makeItems([
+            "こんばんは。",
+            "おやすみなさい。",
+            "さようなら。"
+        ]);
+        $this->builder.close(); // 文章を完成
+    }
 }
