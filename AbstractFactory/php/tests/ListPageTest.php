7ff87caeb1ecc799689c544efe9b5aa3089727e3
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-07-02 16:58:54 +0900

    Add test for ListPage

--- AbstractFactory/php/tests/ListPageTest.php
+++ AbstractFactory/php/tests/ListPageTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase as TestCase;
 
 require_once dirname(__DIR__) . '/listfactory/ListPage.php';
 require_once dirname(__DIR__) . '/listfactory/ListLink.php';
+require_once dirname(__DIR__) . '/listfactory/ListTray.php';
 
 /**
  * ListPage Test
@@ -45,5 +46,43 @@ final class ListPageTest extends TestCase
         $expected .= '</body></html>'."\n";
 
         $this->assertEquals($expected, $page->makeHTML());
+
+        return $page;
+    }
+
+    /**
+     * @depends test_途中にItemを追加するとリスト内にそれを出力する
+     */
+    public function test_トレイでまとめられたものもHTMLに展開される($page)
+    {
+        $asahi = new \listfactory\ListLink("朝日新聞", "http://www.asahi.com");
+        $yomiuri = new \listfactory\ListLink("読売新聞", "http://www.yomiuri.co.jp");
+        $traynews = new \listfactory\ListTray("新聞");
+        $traynews->add($asahi);
+        $traynews->add($yomiuri);
+
+        $page->add($traynews);
+
+        $expected = '';
+        $expected .= '<html><head><title>タイトル</title></head>'."\n";
+        $expected .= '<body>'."\n";
+        $expected .= '<h1>タイトル</h1>'."\n";
+        $expected .= '<ul>'."\n";
+        $expected .= '<li><a href="http://example.com">タイトル(1)</a></li>'."\n";
+        $expected .= '<li><a href="https://example.com">タイトル(2)</a></li>'."\n";
+        $expected .= '<li>' . "\n";
+        $expected .= '新聞' ."\n";
+        $expected .= '<ul>' . "\n";
+        $expected .= '<li><a href="http://www.asahi.com">朝日新聞</a></li>' . "\n";
+        $expected .= '<li><a href="http://www.yomiuri.co.jp">読売新聞</a></li>' . "\n";
+        $expected .= '</ul>' . "\n";
+        $expected .= '</li>' . "\n";
+        $expected .= '</ul>'."\n";
+        $expected .= '<hr><address>所有者</address>'."\n";
+        $expected .= '</body></html>'."\n";
+
+        $this->assertEquals($expected, $page->makeHTML());
+
+        return $page;
     }
 }
