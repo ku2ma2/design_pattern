8bec1eeb4c4c5ab994dd6fd454b4b277f6fc1bce
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-07-07 07:28:49 +0900

    Add Comment

--- Bridge/php/CountDisplay.php
+++ Bridge/php/CountDisplay.php
@@ -4,10 +4,9 @@ require_once "Display.php";
 require_once "DisplayImpl.php";
 
 /**
- * 実装階層の抽象クラス
+ * 機能抽象クラスの拡張
  *
- * 抽象階層(ここではDisplayやCountDisplay)側から移譲されている
- * 「実装階層」側を規定する抽象クラス
+ * 機能階層の抽象クラス(ここではDisplay)を拡張して複数行に対応している。
  *
  * @access public
  * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
@@ -16,11 +15,29 @@ require_once "DisplayImpl.php";
 class CountDisplay extends Display
 {
     private $impl;
+
+    /**
+     * コンストラクタ
+     *
+     * @access public
+     * @param DisplayImpl $impl 表示処理オブジェクト
+     * @return void
+     * @see Display::__construct
+     */
     public function __construct(DisplayImpl $impl)
     {
         parent::__construct($impl);
     }
 
+    /**
+     * 複数行表示
+     *
+     * 与えられた数値分の行数表示する
+     *
+     * @access public
+     * @param int $times 繰り返す数
+     * @return void
+     */
     public function multiDisplay(int $times)
     {
         $this->open();
