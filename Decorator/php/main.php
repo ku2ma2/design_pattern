1308f2fc944a8f4c0b518813ee12e8b177e4698a
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-07-22 09:10:03 +0900

    Finished Decorator pattern

--- Decorator/php/main.php
+++ Decorator/php/main.php
@@ -1,7 +1,33 @@
 <?php
 
-require_once dirname(__DIR__) . '/StringDisplay.php';
-require_once dirname(__DIR__) . '/FullBorder.php';
-require_once dirname(__DIR__) . '/SideBorder.php';
+require_once __DIR__ . '/StringDisplay.php';
+require_once __DIR__ . '/FullBorder.php';
+require_once __DIR__ . '/SideBorder.php';
 
-$b1 = new \Decorator\StringDisplay("Hello,world.");
+use Decorator\StringDisplay;
+use Decorator\SideBorder;
+use Decorator\FullBorder;
+
+$b1 = new StringDisplay("Hello,world.");
+$b2 = new SideBorder($b1, '#');
+$b3 = new FullBorder($b2);
+
+$b1->show();
+$b2->show();
+$b3->show();
+
+$b4 = new SideBorder(
+    new FullBorder(
+        new FullBorder(
+            new SideBorder(
+                new FullBorder(
+                    new StringDisplay("Hello, Border.")
+                ),
+                '*'
+            )
+        )
+    ),
+    '/'
+);
+
+$b4->show();
