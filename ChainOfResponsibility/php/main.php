eb40a7da940036d692a2a3ca4a01668c758a043d
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-08-04 08:04:55 +0900

    Finish Chain of Responsibility

--- ChainOfResponsibility/php/main.php
+++ ChainOfResponsibility/php/main.php
@@ -1,1 +1,23 @@
 <?php
+
+require_once __DIR__ . "/NoSupport.php";
+require_once __DIR__ . "/LimitSupport.php";
+require_once __DIR__ . "/OddSupport.php";
+require_once __DIR__ . "/SpecialSupport.php";
+require_once __DIR__ . "/Trouble.php";
+
+use \ChainOfResponsibility as COR;
+
+$alice  = new COR\NoSupport("Alice");
+$bob    = new COR\LimitSupport("Bob", 100);
+$chalie = new COR\SpecialSupport("Charlie", 429);
+$diana  = new COR\LimitSupport("Diana", 200);
+$elmo   = new COR\OddSupport("Elmo");
+$fred   = new COR\LimitSupport("Fred", 200);
+
+// 邪悪な感じ
+$alice->setNext($bob)->setNext($chalie)->setNext($diana)->setNext($elmo)->setNext($fred);
+
+for ($i=0; $i< 500; $i++) {
+    $alice->support(new COR\Trouble($i));
+}
