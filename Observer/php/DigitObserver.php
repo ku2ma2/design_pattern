a459454a089f241e9c198bf5295ba7d8f65c5f02
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-08-23 07:50:04 +0900

    Add DigitObserver and GraphObserver.php

---
+++ Observer/php/DigitObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Observer;
+
+require_once __DIR__ . '/NumberGenerator.php';
+
+/**
+ * 数字で数を表示するクラス
+ *
+ * @abstract
+ * @access public
+ * @implements SplObjserver
+ * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
+ * @copyright ku2ma2
+ * @see http://php.net/manual/ja/class.splobserver.php
+ */
+class DigitObserver implements \SplObserver
+{
+    public function update(NumberObject $generator)
+    {
+        echo "DigitObserver: {$generator->getNumber()}\n";
+    }
+}
---
+++ Observer/php/GraphObserver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Observer;
+
+require_once __DIR__ . '/NumberGenerator.php';
+
+/**
+ * 数字で数を表示するクラス
+ *
+ * @abstract
+ * @access public
+ * @implements SplObjserver
+ * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
+ * @copyright ku2ma2
+ * @see http://php.net/manual/ja/class.splobserver.php
+ */
+class GraphObserver implements \SplObserver
+{
+    public function update(NumberObject $generator)
+    {
+        echo "GraphObserver: ";
+        $count = $generator->getNumber();
+        for ($i=0; $i< $count; $i++) {
+            echo "*";
+        }
+        echo "\n";
+    }
+}
