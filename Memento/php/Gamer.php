41bdf4675ff081ad2f1c08626067e60387c06b8c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-08-30 06:59:10 +0900

    Add comment in Memento pattern

--- Memento/php/Gamer.php
+++ Memento/php/Gamer.php
@@ -3,7 +3,7 @@
 namespace Memento;
 
 /**
- * 交流チャットルームクラス
+ * ゲームを行う主人公のクラス、Mementoのインスタンスを作る
  *
  * @access public
  * @extends \Mediator\Chatroom
@@ -16,15 +16,40 @@ class Gamer
     private $fruits = [];
     private static $fruitsname = ['りんご','ぶどう','バナナ','みかん'];
 
+    /**
+     * コンストラクタ
+     *
+     * @access public
+     * @param int $money 所持金
+     * @return void
+     */
     public function __construct(int $money)
     {
         $this->money = $money;
     }
+
+    /**
+     * 現在の所持金取得
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function getMoney()
     {
         return $this->money;
     }
 
+    /**
+     * サイコロイベント
+     *
+     * サイコロを振り、その数によって所持金やフルーツが変化する
+     * イベントが起きる
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function bet()
     {
         $dice = mt_rand(1, 6);
@@ -43,6 +68,13 @@ class Gamer
         }
     }
 
+    /**
+     * Memento作成
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function createMemento()
     {
         $m = new \Memento\Memento($this->money);
@@ -56,17 +88,42 @@ class Gamer
         return $m;
     }
 
+    /**
+     * 以前の状態に復帰させる
+     *
+     * @access public
+     * @param Memento $memento 記憶オブジェクト
+     * @return void
+     */
     public function restoreMemento(Memento $memento)
     {
         $this->money = $memento->money;
         $this->fruits = $memento->getFruits();
     }
 
+    /**
+     * 現在の状況を返す
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function __toString()
     {
         $result_fruits = implode(',', $this->fruits);
         return "[money = {$this->money}, fruits = {$result_fruits}]";
     }
+
+    /**
+     * フルーツを取得する
+     *
+     * 乱数によってフルーツが決められる。また、一定の確率でおいしいフルーツを
+     * 取得できる
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     private function getFruit()
     {
         $prefix = '';
--- Memento/php/Memento.php
+++ Memento/php/Memento.php
@@ -15,18 +15,49 @@ class Memento
     public $money;
     private $fruits = [];
 
+    /**
+     * コンストラクタ
+     *
+     * @access public
+     * @param int $money 所持金
+     * @return void
+     */
     public function __construct(int $money)
     {
         $this->money = $money;
     }
+
+    /**
+     * 現在の所持金を取得
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function getMoney()
     {
         return $this->money;
     }
+
+    /**
+     * フルーツを追加
+     *
+     * @access public
+     * @param string $fruit フルーツ名
+     * @return void
+     */
     public function addFruit(string $fruit)
     {
         $this->fruits[] = $fruit;
     }
+
+    /**
+     * フルーツリストを取得
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function getFruits()
     {
         return $this->fruits;
