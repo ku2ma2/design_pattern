8fe5db69e32b500f266fb78c4df588b2380081f1
Hiro Kunimatsu <motorohi.tsumaniku+bitbucket@gmail.com>
2017-09-04 09:24:46 +0000

    Finish State pattern

--- State/php/AuthorizedState.php
+++ State/php/AuthorizedState.php
@@ -5,9 +5,9 @@ namespace State;
 require_once __DIR__ . '/UserState.php';
 
 /**
- * Stateを実装しているクラス。昼間の状態を表す
+ * UserStateを実装しているクラス。認証されている状態を表わす
  *
- * @implements State
+ * @implements UserState
  * @access public
  * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
  * @copyright ku2ma2
@@ -38,8 +38,6 @@ class AuthorizedState implements UserState
      * @access public
      * @param void
      * @return object self::$ingleton ただ一つのインスタンス
-     * @see Class::function
-     * @todo TODO
      */
     public static function getInstance()
     {
@@ -48,14 +46,38 @@ class AuthorizedState implements UserState
         }
         return self::$singleton;
     }
+
+    /**
+     * 認証されているかどうか
+     *
+     * @access public
+     * @param void
+     * @return bool
+     */
     public function isAuthenticated()
     {
         return true;
     }
+
+    /**
+     * 次の状態へ遷移
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function nextState()
     {
         return UnauthorizedState::getInstance();
     }
+
+    /**
+     * メニューの取得
+     *
+     * @access public
+     * @param void
+     * @return void
+     */
     public function getMenu()
     {
         $menu = '<a href="?mode=inc">カウントアップ</a> | ';
@@ -66,7 +88,14 @@ class AuthorizedState implements UserState
     }
 
     /**
-     * このインスタンスの複製を許可しないようにする
+     * クローンを禁止
+     *
+     * このインスタンスの複製を許可しないようにしている。
+     *
+     * @final
+     * @access public
+     * @param void
+     * @return void
      * @throws RuntimeException
      */
     final public function __clone()
@@ -74,6 +103,13 @@ class AuthorizedState implements UserState
         throw new RuntimeException('Clone is not allowed against ' . get_class($this));
     }
 
+    /**
+     * 文字列表現
+     *
+     * @access public
+     * @param void
+     * @return string
+     */
     public function __toString()
     {
         return "[認証済み]";
