50031c24d95dc217726fbdcb9c76516910e5e099
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-09-04 09:15:55 +0900

    Improve Login.php

--- State/php/Login.php
+++ State/php/Login.php
@@ -12,4 +12,45 @@ namespace State;
  */
 class Login implements Context
 {
+    private $name;
+    private $state;
+    private $count = 0;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->state = UnauthorizedState::getInstance();
+        $this->resetCount();
+    }
+    public function switchState()
+    {
+        echo "状態遷移" . get_class($this->state) . "→";
+        $this->state = $this->state->nextState();
+        echo get_class($this->state) . "<br>";
+        $this->resetCount();
+    }
+    public function isAuthenticated()
+    {
+        return $this->state->isAuthenticated();
+    }
+    public function getMenu()
+    {
+        return $this->state->getMenu();
+    }
+    public function getUserName()
+    {
+        return $this->name;
+    }
+    public function getCount()
+    {
+        return $this->count;
+    }
+    public function incrementCount()
+    {
+        $this->count++;
+    }
+    public function resetCount()
+    {
+        $this->count = 0;
+    }
 }
