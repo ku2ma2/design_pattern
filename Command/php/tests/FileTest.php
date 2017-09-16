adb6cf43211f579a1e620b49dbc65079785a7331
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-09-16 09:02:02 +0900

    Add File.php and test in command pattern

--- Command/php/tests/FileTest.php
+++ Command/php/tests/FileTest.php
@@ -10,8 +10,37 @@ final class CommandTestTest extends TestCase
 {
     public function test_getName_名前を取得する()
     {
-        $expected = '';
-        $actual = '';
+        $expected = '太郎';
+
+        $file = new \Command\File('太郎');
+        $actual = $file->getName();
+        $this->assertEquals($expected, $actual);
+    }
+    final public function test_decompress_解凍展開する()
+    {
+        $expected = "次郎を展開しました\n";
+
+        $file = new \Command\File('次郎');
+        $actual = $file->decompress();
+
+        $this->assertEquals($expected, $actual);
+    }
+    final public function test_compress_圧縮する()
+    {
+        $expected = "三郎を圧縮しました\n";
+
+        $file = new \Command\File('三郎');
+        $actual = $file->compress();
+
+        $this->assertEquals($expected, $actual);
+    }
+    final public function test_create_作成する()
+    {
+        $expected = "四郎を作成しました\n";
+
+        $file = new \Command\File('四郎');
+        $actual = $file->create();
+
         $this->assertEquals($expected, $actual);
     }
 }
