3b878452fb04644685e1d17cb46573c9a4585309
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-09-19 07:46:32 +0900

    Add CommandLIstNode.php in interpreter pattern

--- Interpreter/php/ProgramNode.php
+++ Interpreter/php/ProgramNode.php
@@ -3,11 +3,23 @@
 namespace Interpreter;
 
 require_once __DIR__.'/Node.php';
+require_once __DIR__.'/Context.php';
 
+/**
+ * <program>に対応するクラス
+ *
+ * ex)
+ * <program> ::= program <command list>
+ *
+ * @access public
+ * @extends Node
+ * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
+ * @copyright ku2ma2
+ */
 class ProgramNode extends Node
 {
     private $commandListNode;
-    
+
     public function parse(Context $context)
     {
         $context->skipToken("program");
