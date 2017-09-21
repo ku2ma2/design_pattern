c06d5414407f963b7fb14b65380e0a42ca1247d9
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-09-22 08:05:06 +0900

    Update Context.php

--- Interpreter/php/Context.php
+++ Interpreter/php/Context.php
@@ -12,4 +12,34 @@ namespace Interpreter;
  */
 class Context
 {
+    private $token;
+    private $currentToken;
+
+    public function __construct(string $text)
+    {
+        $this->token = preg_split("/[\s,]+/", $text);
+        $this->nextToken();
+    }
+
+    public function nextToken()
+    {
+        $this->currentToken = ($this->currentToken === null)
+            ? current($this->token)
+            : next($this->token);
+    }
+    public function currentToken(): string
+    {
+        return $this->currentToken();
+    }
+    public function skipToken(string $token)
+    {
+        if ($token !== $this->currentToken) {
+            throw new ParseException("Warning: {$token} is expected, but {$this->currentToken} is found.");
+        }
+        $this->nextToken();
+    }
+    public function currentNumber(): int
+    {
+        return intval($this->currentToken);
+    }
 }
