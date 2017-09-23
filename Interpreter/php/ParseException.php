0f48b68a910453916cbca80c70714de6218f042b
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-09-24 08:47:23 +0900

    Add comment

--- Interpreter/php/ParseException.php
+++ Interpreter/php/ParseException.php
@@ -2,6 +2,14 @@
 
 namespace Interpreter;
 
+/**
+ * 構文解析中の例外クラス
+ *
+ * @access public
+ * @extends \Exception
+ * @author ku2ma2 <motorohi.tsumaniku@gmail.com>
+ * @copyright ku2ma2
+ */
 class ParseException extends \Exception
 {
     public function __construct(string $msg)
